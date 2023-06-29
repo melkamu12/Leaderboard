ee03272966bdd8db9c813a93df6092dcd5407d4c
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-06-29 10:23:22 +0300

    data send and receive in API

--- src/module/gameAPI.js
+++ src/module/gameAPI.js
@@ -1,3 +1,2 @@
-const url =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdn/scores";
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdn/scores';
 export default url;
